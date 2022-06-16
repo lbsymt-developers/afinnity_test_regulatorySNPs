9c39e0ffcd426a687424bc0e18c8485792502270
Erickcufe <erickcufe94@gmail.com>
2022-06-16 13:34:39 -0500

    .

--- R/atSNP.R
+++ R/atSNP.R
@@ -13,4 +13,7 @@ data(jaspar_library)
 jaspar_motif[[1]]
 jaspar_motifinfo[names(jaspar_motif[1])]
 
-snp_info1 <- LoadSNPData(snpids = c("rs5050", "rs616488", "rs11249433", "rs182799", "rs12565013", "rs11208590"), genome.lib ="BSgenome.Hsapiens.UCSC.hg38", snp.lib = "SNPlocs.Hsapiens.dbSNP151.GRCh38", half.window.size = 30, default.par = TRUE, mutation = FALSE)
+snp_info1 <- LoadSNPData(snpids = c("rs5050", "rs616488", "rs11249433", "rs182799", "rs12565013", "rs11208590"),
+                         genome.lib ="BSgenome.Hsapiens.UCSC.hg38",
+                         snp.lib = "SNPlocs.Hsapiens.dbSNP155.GRCh38", half.window.size = 30, default.par = TRUE, mutation = FALSE)
+

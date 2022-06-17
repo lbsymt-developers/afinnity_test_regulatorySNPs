381d915c3ca0182a4d0a7de8bc90f10f304f64a1
Erickcufe <erickcufe94@gmail.com>
2022-06-16 19:35:51 -0500

    .

--- R/atSNP.R
+++ R/atSNP.R
@@ -13,23 +13,26 @@ data(jaspar_library)
 jaspar_motif[[1]]
 jaspar_motifinfo[names(jaspar_motif[1])]
 
-snp_info1 <- LoadSNPData(snpids = c("rs5050", "rs616488", "rs11249433", "rs182799", "rs12565013", "rs11208590"),
+snp_info1 <- LoadSNPData(snpids = c("rs405509", "rs769446", "rs449647"),
                          genome.lib ="BSgenome.Hsapiens.UCSC.hg38",
                          snp.lib = "SNPlocs.Hsapiens.dbSNP155.GRCh38", half.window.size = 30, default.par = TRUE, mutation = FALSE)
 
-install.packages("../../../Downloads/SNPlocs.Hsapiens.dbSNP155.GRCh38_0.99.21.tar",
-                 repos = NULL, type = "source")
+# install.packages("../../../Downloads/SNPlocs.Hsapiens.dbSNP155.GRCh38_0.99.21.tar",
+#                  repos = NULL, type = "source")
 
 str(snp_info1)
 data(encode_library)
 encode_motifinfo[names(encode_motif)]
 
-atsnp.scores <- ComputeMotifScore(encode_motif, snp_info1, ncores = 3)
+atsnp.scores <- ComputeMotifScore(encode_motif, snp_info1,
+                                  ncores = 3)
 atsnp.scores$snp.tbl
 atsnp.scores$motif.scores
 
-atsnp.result <- ComputePValues(motif.lib = encode_motif, snp.info = snp_info1,
-                               motif.scores = atsnp.scores$motif.scores, ncores = 3, testing.mc=TRUE)
+atsnp.result <- ComputePValues(motif.lib = encode_motif,
+                               snp.info = snp_info1,
+                               motif.scores = atsnp.scores$motif.scores,
+                               ncores = 3, testing.mc=TRUE)
 atsnp.result
 head(atsnp.result[order(atsnp.result$pval_rank), c("snpid", "motif", "pval_ref", "pval_snp", "pval_rank")])
 
@@ -37,18 +40,28 @@ library(qvalue)
 qval_rank = qvalue(atsnp.result$pval_rank, pi0=0.1)$qvalues
 fdr <- p.adjust(atsnp.result$pval_snp)
 
-atsnp.result = cbind(atsnp.result, qval_rank)
-head(atsnp.result[order(atsnp.result$pval_rank), c("snpid", "motif", "pval_ref", "pval_snp", "pval_rank")])
+
+atsnp.result = cbind(atsnp.result, qval_rank, fdr)
+head(atsnp.result[order(atsnp.result$pval_rank),
+                  c("snpid", "motif", "pval_ref", "pval_snp", "pval_rank", "fdr")])
 
 
 match.subseq_result <- MatchSubsequence(snp.tbl = atsnp.scores$snp.tbl,
                                         motif.scores = atsnp.result, motif.lib = encode_motif,
-                                        snpids = c("rs5050", "rs12565013"),
+                                        snpids = c("rs769446"),
                                         motifs = names(encode_motif), ncores = 1)
 match.subseq_result[c("snpid", "motif", "IUPAC", "ref_match_seq", "snp_match_seq")]
 match.seq <- dtMotifMatch(atsnp.scores$snp.tbl,
                           atsnp.scores$motif.scores,
-                          snpids="rs5050", motifs="CEBPB_1",
+                          snpids="rs769446", motifs="ELF1_disc3",
                           motif.lib = encode_motif)
 match.seq <- match.seq[!duplicated(match.seq$snpid),]
 plotMotifMatch(match.seq,  motif.lib = encode_motif)
+
+
+match.seq_2 <- dtMotifMatch(atsnp.scores$snp.tbl,
+                          atsnp.scores$motif.scores,
+                          snpids="rs405509", motifs="POU2F3_2",
+                          motif.lib = encode_motif)
+match.seq <- match.seq[!duplicated(match.seq$snpid),]
+plotMotifMatch(match.seq_2,  motif.lib = encode_motif)

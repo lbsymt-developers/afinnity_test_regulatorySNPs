ff56664b26a6c7462b613af9e1ed56e9be532989
Erickcufe <erickcufe94@gmail.com>
2023-01-27 12:38:08 -0600

    .

--- R/motifbreak.R
+++ R/motifbreak.R
@@ -25,14 +25,56 @@ results <- motifbreakR::motifbreakR(snpList = snps.mb, filterp = TRUE,
                        BPPARAM = BiocParallel::MulticoreParam(7))
 
 saveRDS(results, "results/motifs_all_AD_SNPs.rds")
+results <- readRDS("results/motifs_all_AD_SNPs.rds")
+
+find_critical_SNPs <- function(results){
+  suppressMessages(library(dplyr))
+  df <- results
+  df <- df[df$effect == "strong", ]
+  snps <- unique(names(df))
+
+  future::plan("multisession")
+  aa <- purrr::map(snps, purrr::safely(function(x){
+    print(x)
+    df_tmp <- df[names(df) %in% x,] %>%
+      motifbreakR::calculatePvalue() %>%
+      data.frame() %>%
+      filter(Refpvalue <= 0.05)
+  }), .progress = TRUE)
+
+
+
+  df_snps <- data.frame()
+  for(i in 1:length(snps)){
+    print(i)
+    df_tmp <- df[names(df) %in% snps[i],] %>%
+      motifbreakR::calculatePvalue() %>%
+      data.frame() %>%
+      filter(Refpvalue <= 0.05)
+    df_snps <- rbind(df_tmp, df_snps)
+  }
+  return(df_snps)
+}
+
+
+results_snps <- find_critical_SNPs(results)
+
+r_strong <- results[results$effect=="strong"]
+rs1001158 <- results[names(results) %in% "rs1001158:A"]
+rs1001158
+rs1001158 <- calculatePvalue(rs1001158)
+rs1001158[rs1001158$Altpvalue<=0.05]
+
+
+saveRDS(results_pvalue, "results/motifs_all_AD_SNPs_PVALUE.rds")
 rs13032148 <- results[names(results) %in% "rs13032148"]
-rs13032148
+
 rs13032148 <- calculatePvalue(rs13032148)
-rs13032148
+
 
 library(BSgenome.Hsapiens.UCSC.hg38)
 
-plotMB(results = results, rsid = "rs13032148", effect = "strong")
+plotMB(results = results, rsid = "rs1001158:A", effect = "strong")
 
 
 rs1060743 <- results[names(results) %in% "rs1060743"]
